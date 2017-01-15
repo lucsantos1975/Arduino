dd91e3fd3671907ec6f67f36bc88048fafd7ef02
Luciano Santos <luc.santos1975@gmail.com>
2017-01-15 10:30:57 -0200

    Laser diode test. DHT11 temperature sensor + LCD 16x2

---
+++ sketchbook/dht11_lcd16x2/dht11_lcd16x2.ino
@@ -0,0 +1,52 @@
+#include <DHT.h>
+#include <LiquidCrystal.h>
+ 
+#define DHTPIN A1
+#define DHTTYPE DHT11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup() {
+    lcd.begin(16, 2);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Teste do sensor");
+    
+    dht.begin();
+    
+    delay(5000);
+}
+
+void loop() {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    
+    if (isnan(t) || isnan(h)) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Falha de leitura");
+        
+        delay(5000);
+    } else {
+        lcd.clear();
+        
+        lcd.setCursor(0, 0);
+        lcd.print("Umid.:");
+        lcd.setCursor(7, 0);
+        lcd.print(h);
+        lcd.setCursor(15, 0);
+        lcd.print("%");
+
+        lcd.setCursor(0, 1);
+        lcd.print("Temp.:");
+        lcd.setCursor(7, 1);
+        lcd.print(t);
+        lcd.setCursor(14, 1);
+        lcd.write(B11011111);
+        lcd.setCursor(15, 1);
+        lcd.print("C");
+    }
+  
+    delay(10000);
+}
